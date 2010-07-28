b7726cd1beed5a9945a65746c260dca2310de9d5
NgTzeYang <ngty77@gmail.com>
2010-07-28 23:19:26 +0800

    Use multivalent instead of pdftk, since we need java solution for pdf comparion (have yet to find a suitable non-java tool).

--- lib/gjman/pdf/compressor.rb
+++ lib/gjman/pdf/compressor.rb
@@ -4,26 +4,30 @@ module Gjman
       class << self
 
         def do(src, dest)
-          pdftk(:compress, src, dest)
-          # multivalent(:Compress, src, dest, src.sub(/\.pdf$/, '-o.pdf'))
+          #pdftk(:compress, src, dest)
+          multivalent(:Compress, src, dest, src.sub(/\.pdf$/, '-o.pdf'))
         end
 
         def undo(src, dest)
-          pdftk(:uncompress, src, dest)
-          # multivalent(:Uncompress, src, dest, src.sub(/\.pdf$/, '-u.pdf'))
+          #pdftk(:uncompress, src, dest)
+          multivalent(:Uncompress, src, dest, src.sub(/\.pdf$/, '-u.pdf'))
         end
 
         private
 
          def pdftk(mode, src, dest)
-           debug = shell(:pdftk, [src, :output, dest, mode, :verbose])
-           $stdout.puts '', debug if Gjman.verbose
+           shell(:pdftk, [src, :output, dest, mode, :verbose])
            dest
          end
 
          def multivalent(mode, src, dest, tmp_dest)
-           java(:multivalent, %W{tool.pdf.#{mode} #{src}})
-           File.move(tmp_dest, dest)
+           case java(:multivalent, %W{tool.pdf.#{mode} #{src}})
+           when /Already compressed\.  \(Force recompression with \-force\.\)/,
+             /java\.lang\.ArrayIndexOutOfBoundsException/
+             File.copy(src, dest)
+           else
+             File.move(tmp_dest, dest)
+           end
            dest
          end
 
