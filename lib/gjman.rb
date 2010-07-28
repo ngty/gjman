75c9c4ed900340740811aef30c718802aac4f086
NgTzeYang <ngty77@gmail.com>
2010-07-28 17:09:55 +0800

    Gjman.root is used in required files even before its declaration.

--- lib/gjman.rb
+++ lib/gjman.rb
@@ -1,1 +1,14 @@
+module Gjman
+
+  ROOT = File.join(File.expand_path(File.dirname(__FILE__)))
+
+  class << self
+    def root(*args)
+      args.size == 0 ? ROOT : File.join(ROOT, *args)
+    end
+  end
+
+end
+
 require 'gjman/file_system'
+require 'gjman/pdf'
