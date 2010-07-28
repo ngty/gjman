cea64261611316492fe45006ad44c49f260a5486
NgTzeYang <ngty77@gmail.com>
2010-07-28 22:17:55 +0800

    Added basic configuration support.

--- lib/gjman.rb
+++ lib/gjman.rb
@@ -2,11 +2,26 @@ module Gjman
 
   ROOT = File.join(File.expand_path(File.dirname(__FILE__)))
 
+  DEFAULTS = {
+    :verbose => true
+  }
+
   class << self
+
+    attr_accessor *DEFAULTS.keys
+
     def root(*args)
       args.size == 0 ? ROOT : File.join(ROOT, *args)
     end
+
+    def configure(args = {}, &block)
+      args.each{|name, val| send(:"#{name}=", val) }
+      block.call(self) if block_given?
+    end
+
   end
+
+  configure(DEFAULTS)
 
 end
 
