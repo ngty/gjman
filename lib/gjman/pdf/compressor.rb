7e221f16d46c85832c783fe5e643a826cd7f3691
NgTzeYang <ngty77@gmail.com>
2010-07-28 23:23:45 +0800

    Explain why we choose multivalent over pdftk.

--- lib/gjman/pdf/compressor.rb
+++ lib/gjman/pdf/compressor.rb
@@ -4,21 +4,14 @@ module Gjman
       class << self
 
         def do(src, dest)
-          #pdftk(:compress, src, dest)
           multivalent(:Compress, src, dest, src.sub(/\.pdf$/, '-o.pdf'))
         end
 
         def undo(src, dest)
-          #pdftk(:uncompress, src, dest)
           multivalent(:Uncompress, src, dest, src.sub(/\.pdf$/, '-u.pdf'))
         end
 
         private
-
-         def pdftk(mode, src, dest)
-           shell(:pdftk, [src, :output, dest, mode, :verbose])
-           dest
-         end
 
          def multivalent(mode, src, dest, tmp_dest)
            case java(:multivalent, %W{tool.pdf.#{mode} #{src}})
@@ -30,6 +23,24 @@ module Gjman
            end
            dest
          end
+
+          # NOTE: Since pdftk is almost as dead as multivalent (last updated in 2006),
+          # there is really no good reason to choose it over multivalent. Moreover,
+          # since pdf comparison requires java solution, it makes sense to be consistent,
+          # which is essentially sticking to java.
+          #
+          # def do(src, dest)
+          #   pdftk(:compress, src, dest)
+          # end
+          #
+          # def undo(src, dest)
+          #   pdftk(:uncompress, src, dest)
+          # end
+          #
+          # def pdftk(mode, src, dest)
+          #   shell(:pdftk, [src, :output, dest, mode, :verbose])
+          #   dest
+          # end
 
       end
     end
