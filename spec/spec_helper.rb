d8016279aa21a32f5e849f6130100accd3cdc160
NgTzeYang <ngty77@gmail.com>
2010-07-28 17:10:13 +0800

    Useful methods for working with tmp files.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,13 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'gjman'
 
 Bacon.summary_on_exit
+
+def trash_tmp_files
+  ($trashable_tmp_files || []).select {|f| f.path }.map(&:unlink)
+  $trashable_tmp_files = nil
+end
+
+def tmp_file(file_name)
+  (($trashable_tmp_files ||= []) << Tempfile.new(file_name))[-1]
+end
+
