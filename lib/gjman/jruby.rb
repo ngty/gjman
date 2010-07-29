8a8e8548b9376173bed957f09c90a77f43e6ac43
NgTzeYang <ngty77@gmail.com>
2010-07-30 03:40:55 +0800

    Generic jruby support.

---
+++ lib/gjman/jruby.rb
@@ -0,0 +1,34 @@
+module Gjman
+  module JRuby
+    class << self
+
+      def initialize
+        @initialized ||= (
+          include Java
+          Gjman::JARS.join(':').split(':').each{|jar| require jar }
+          $CLASSPATH << root('gjman','hack')
+          java_import 'ForbidSystemExit'
+          true
+        )
+      end
+
+      def classify(klass)
+        java_import klass
+        Java.send(klass)
+      end
+
+      def sandbox(&block)
+        initialize
+        begin
+          ForbidSystemExit.apply
+          @result = yield
+        rescue ForbidSystemExit::Exception
+          @result
+        ensure
+          ForbidSystemExit.unapply
+        end
+      end
+
+    end
+  end
+end
