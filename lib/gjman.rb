9ef31b7058137c337bd72f286338f1df87d7927f
NgTzeYang <ngty77@gmail.com>
2010-07-30 03:39:19 +0800

    Cleaning up, pulled out rjb stuff that should belong elsewhere.

--- lib/gjman.rb
+++ lib/gjman.rb
@@ -1,8 +1,11 @@
 require 'gjman/file_system'
+require 'gjman/jruby'
+require 'gjman/rjb'
 
 module Gjman
 
   ROOT = File.join(File.expand_path(File.dirname(__FILE__)))
+
   JARS, JMODE = [],
     case RUBY_PLATFORM
     when /java/i then :JRuby
@@ -10,16 +13,9 @@ module Gjman
     end
 
   class << self
-
     def root(*args)
       args.size == 0 ? ROOT : File.join(ROOT, *args)
     end
-
-    def rjb_load
-      Rjb::load(JARS.join(':')) unless @rjb_loaded
-      @rjb_loaded = true
-    end
-
   end
 
 end
