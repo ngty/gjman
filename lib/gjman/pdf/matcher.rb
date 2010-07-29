c35f687cf509383be9455139b4edab971a7003ec
NgTzeYang <ngty77@gmail.com>
2010-07-29 11:09:43 +0800

    Spec for different image that are positioned & sized the same is passing.

--- lib/gjman/pdf/matcher.rb
+++ lib/gjman/pdf/matcher.rb
@@ -6,7 +6,11 @@ module Gjman
         def test(pdf_x, pdf_y)
           begin
             tmp_x, tmp_y = uncompress(pdf_x, pdf_y)
-            same_fonts?(tmp_x, tmp_y) && same_contents?(tmp_x, tmp_y)
+            [
+              same_fonts?(tmp_x, tmp_y),
+              same_images?(tmp_x, tmp_y),
+              same_contents?(tmp_x, tmp_y),
+            ].all?
           ensure
             FileSystem.trash_tmp_files
           end
